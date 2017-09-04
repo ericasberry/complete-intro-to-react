1ec36f0ca86b0ee61f89dd5d542e65fff1636685
Eric Asberry <eric@ericasberry.com>
2017-09-03 22:33:22 -0400

    hot module reload

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Landing from "./Landing";
-import Search from "./Search";
+import App from "./App";
 
-const FourOhFour = () => <h1>404</h1>;
+const renderApp = () => {
+  render(<App />, document.getElementById("app"));
+};
 
-// className instead of class because class is a reserved word
-// in Javascript, and this is JSX ... not pure html
-// Switch = render exactly one component; first that matches
-const App = () => (
-  <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={Search} />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+renderApp();
 
-render(<App />, document.getElementById("app"));
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    renderApp();
+  });
+}
