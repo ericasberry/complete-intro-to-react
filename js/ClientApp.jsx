2261c217d07228ffa5348637ceb4a4bcf978a089
Eric Asberry <eric@ericasberry.com>
2017-09-04 21:26:05 -0400

    end of react before redux

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,16 +1,20 @@
 // @flow
 
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
+import Perf from "react-addons-perf";
+import App from "./App";
+
+window.Perf = Perf;
+Perf.start();
 
 const renderApp = () => {
-  render(<App />, document.getElementById('app'));
+  render(<App />, document.getElementById("app"));
 };
 renderApp();
 
 if (module.hot) {
-  module.hot.accept('./App', () => {
+  module.hot.accept("./App", () => {
     renderApp();
   });
 }
